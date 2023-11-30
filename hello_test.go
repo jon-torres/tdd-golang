d93d99d34ce44c1c401ecb3a56d83dbb5a24d360
jon-torres <jonathantorres10@gmail.com>
2023-11-30 11:38:26 -0300

    refactored tests - DRY

--- hello_test.go
+++ hello_test.go
@@ -6,17 +6,17 @@ func TestHello(t *testing.T) {
 	t.Run("Saying 'olá' to people", func(t *testing.T) {
 		got := Hello("Jon")
 		want := "Olá, Jon"
-
-		if got != want {
-			t.Errorf("git %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say 'Olá, Mundo' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Olá, Mundo"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
