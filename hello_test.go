4985a7c54e91864f1f37801e9090d3430caba363
jon-torres <jonathantorres10@gmail.com>
2023-11-30 11:28:53 -0300

    added a test for empty strings

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Jon")
-	want := "Olá, Jon"
+	t.Run("Saying 'olá' to people", func(t *testing.T) {
+		got := Hello("Jon")
+		want := "Olá, Jon"
 
-	if got != want {
-		t.Errorf("git %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("git %q want %q", got, want)
+		}
+	})
+	t.Run("Say 'Olá, Mundo' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Olá, Mundo"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
